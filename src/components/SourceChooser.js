14870fcd8ac742dc8d2c3ed1aec52f360bb0f719
Romain Guillemot <wasthishelpful@gmail.com>
2019-08-14 11:18:24 +0200

    added a state to SourceChooser to handle input value

--- src/components/SourceChooser.js
+++ src/components/SourceChooser.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {UID} from 'react-uid'
 
 const inlineStyle = {
@@ -8,19 +8,23 @@ const stretchInsetStyle = {
   padding: `.5rem .25rem`
 }
 
-const SourceChooser = () => (
-  <form>
-    <UID>
-      {
-        id => (
-          <div>
-            <label htmlFor={id} style={inlineStyle}>Type a GitHub username:</label>
-            <input id={id} style={stretchInsetStyle} type="text" placeholder="jdoe" />
-          </div>
-        )
-      }
-    </UID>
-  </form>
-)
+const SourceChooser = () => {
+  const [source, setSource] = useState("")
+
+  return (
+    <form>
+      <UID>
+        {
+          id => (
+            <div>
+              <label htmlFor={id} style={inlineStyle}>Type a GitHub username:</label>
+              <input id={id} style={stretchInsetStyle} type="text" placeholder="jdoe" value={source} onChange={(event) => setSource(event.target.value)} />
+            </div>
+          )
+        }
+      </UID>
+    </form>
+  )
+}
 
 export default SourceChooser
