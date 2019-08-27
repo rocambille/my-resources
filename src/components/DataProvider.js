f246ea9fca59e9441841809728a6be30ce5daec4
Romain Guillemot <wasthishelpful@gmail.com>
2019-08-27 08:51:37 +0200

    refactored fetch code in DataProvider using async/await

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -5,33 +5,37 @@ import 'styles/space.css'
 
 const DataProvider = ({dataState}) => {
 
-  const fetchUsername = (username) => {
+  const fetchUsername = async (username) => {
     if (!username || username.length === 0) {
       return
     }
 
-    fetch(
+    const response = await fetch(
       `https://api.github.com/repos/${username}/my-resources/contents/db.json`
-    ).then(
-      response => response.json()
-    ).then(
-      json => {
-        // thx https://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings
-        const fileContent = decodeURIComponent(
-          atob(
-            json.content
-          ).split('').map(
-            function(c) {
-              return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-            }
-          ).join('')
-        )
-
-        dataState.set(JSON.parse(fileContent))
-
-        window.localStorage.setItem('lastFetchedUsername', username)
-      }
     )
+
+    if (response.status !== 200) {
+      return
+    }
+
+    window.localStorage.setItem('lastFetchedUsername', username)
+
+    const json = await response.json()
+
+    // thx https://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings
+    const data = JSON.parse(
+      decodeURIComponent(
+        atob(
+          json.content
+        ).split('').map(
+          function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+          }
+        ).join('')
+      )
+    )
+
+    dataState.set(data)
   }
 
   const lastFetchedUsername = window.localStorage.getItem('lastFetchedUsername') || ''
