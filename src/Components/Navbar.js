19a694a7c60ae367658b30ee7f9f8e786b77cac3
Baedyl <baedyl@localhost.localdomain>
2019-10-01 17:15:30 +0100

    Added a responsive nav bar and some colors

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,39 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink
+import '../Styles/navbar.css';
+
+const Navbar = () => {
+  const beResponsive = () => {
+    let x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+  } 
+  return (
+    <div id="topnav" className="topnav">
+       <NavLink
           to="/resources"
+          className="button"
           activeClassName="nav__link_active"
           >
           Home
         </NavLink>
-      </li>
-      <li>
         <NavLink
           to="/resources/add"
+          className="button"
           activeClassName="nav__link_active"
           >
           Add resource
         </NavLink>
-      </li>
-    </ul>
-  </nav>
-)
-
+        <a href="#" className="icon" onClick={ beResponsive }>
+          <FontAwesomeIcon icon={ faBars } />
+        </a>
+    </div>
+  ); 
+}
 export default Navbar
