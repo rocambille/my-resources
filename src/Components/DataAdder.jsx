250a00674381762a4b2eebbea15f80ff8b3c96a0
Romain Guillemot <wasthishelpful@gmail.com>
2019-11-16 18:34:52 +0100

    used react-use-form-state in DataAdder

--- src/Components/DataAdder.jsx
+++ src/Components/DataAdder.jsx
@@ -1,9 +1,38 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react'
+import { useFormState } from 'react-use-form-state'
 
-const DataAdder = () => (
-  <>
-    <p>add data</p>
-  </>
-)
+const DataAdder = () => {
+  const [
+    formState,
+    {
+      url,
+    },
+  ] = useFormState(null, { withIds: true })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(formState.values.uri)
+  }
+
+  return (
+    <form
+      className="_space-size_s"
+      onSubmit={handleSubmit}
+    >
+      <label
+        {...url('uri')}
+        className="_space_inline"
+      >
+        URI
+      </label>
+      <input
+        {...url('uri')}
+        className="_space-size_xs _space_inset-stretch"
+      />
+    </form>
+  )
+}
 
 export default DataAdder
